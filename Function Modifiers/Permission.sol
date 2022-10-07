ef8f33ced65b64908e893c939bdddca1b56f9b44
Dafne <villarroelsol1@gmail.com>
2022-10-07 16:23:37 -0300

    deploy

---
+++ Function Modifiers/Permission.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Permission {
+    address private owner;
+    string public projectName = "Dafne";
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can change the project name");
+        _;
+    }
+
+    function changeProjectName(string memory _projectName) public onlyOwner {
+        projectName = _projectName;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#crowdfunding
+You can see the deploy contract in Goerli Network 0x61C8F5d697cf09e91f975E48c8EB1cb74976d537 Contract Address
